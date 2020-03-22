899375e5d4103551db433ca3e34a1a237c338511
Ole <ole@nocarrier.de>
2020-03-22 14:22:14 +0100

    Register Frontend

---
+++ BlazorFood/UserApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFood
+{
+    public class UserApi
+    {
+        public void Register(Models.RegisterUser registerdata)
+        {
+
+            Console.WriteLine("Im backend digger");
+        }
+    }
+}
