ebb66087af1339d6bee29510b2b0ea97f4c9be72
Ole <ole@nocarrier.de>
2020-04-18 15:53:30 +0200

    Fixes UserAdres

--- BleifoodWeb/Models/RegisterUser.cs
+++ BleifoodWeb/Models/RegisterUser.cs
@@ -8,6 +8,8 @@ namespace Bleifood.Web.Models
 {
     public class RegisterUser
     {
+        public bool IsTrue { get; } = true; // VERY dirty :)
+
         [Required(ErrorMessage = "Bitte nicht leer lassen"), EmailAddress(ErrorMessage ="Dies ist keine gültige E-Mail"), DataType(DataType.EmailAddress)]
         public string Mail { get; set; }
         [Required(ErrorMessage ="Bitte nicht leer lassen"), MinLength(6, ErrorMessage ="Das Passwort muss mindestens 6 Stellen besitzen"), DataType(DataType.Password)]
@@ -15,5 +17,7 @@ namespace Bleifood.Web.Models
         [Required(ErrorMessage = "Bitte nicht leer lassen"), Compare(nameof(Password), ErrorMessage ="Die Passwörter sind nicht gleich"), DataType(DataType.Password)]
         public string PasswordRepeat { get; set; }
         public string LastError { get; set; }
+        [Compare(nameof(IsTrue), ErrorMessage ="Muss angehakt sein")]
+        public bool Agb { get; set; }
     }
 }
